a48ece2d8d1687b973010af21b058425901c1714
Guilherme <guilhermemagno09@gmail.com>
2023-03-28 13:10:14 -0300

    adding in animetedOpacity

---
+++ lib/pages/animation_opacity.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class OpacityAnimation extends StatefulWidget {
+  const OpacityAnimation({super.key});
+
+  @override
+  State<OpacityAnimation> createState() => _OpacityAnimationState();
+}
+
+class _OpacityAnimationState extends State<OpacityAnimation> {
+  bool _visible = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: AnimatedOpacity(
+          duration: const Duration(seconds: 1),
+          opacity: _visible ? 1 : 0,
+          curve: Curves.elasticIn,
+          child: Container(
+            width: 100,
+            height: 100,
+            color: Colors.amber,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _visible = !_visible;
+          });
+        },
+        tooltip: 'Toggle Opacity',
+        child: const Icon(Icons.flip),
+      ),
+    );
+  }
+}
